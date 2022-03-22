b47afb4c773dc4c4437a683396618f54c3d287b1
arun-web-dev <miuiandrew@gmail.com>
2022-03-22 14:47:50 +0530

    recipe feature added

--- src/js/controller.js
+++ src/js/controller.js
@@ -29,9 +29,11 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
     renderSpinner(recipeContainer);
     const res = await fetch(
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
     const data = await res.json();
     // 1) loading recipe
@@ -157,4 +159,4 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
